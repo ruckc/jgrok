d7082f70a87574ef0aac9d08b4c366afeffa2037
ruckc <ruckc@yahoo.com>
2015-05-10 07:23:32 -0600

    fix logging issue

--- src/main/java/io/ruck/jgrok/Grok.java
+++ src/main/java/io/ruck/jgrok/Grok.java
@@ -58,7 +58,7 @@ public class Grok {
     }
 
     private static Grok compile(String original, String grokpattern, Map<String, String> variables, Patterns patterns) {
-        log.log(Level.INFO, "compiling pattern {0} => {1}", new Object[]{original, grokpattern});
+        log.log(Level.INFO, "compiling pattern {} => {}", original, grokpattern);
         Matcher m = GROK_SUBSTITUTION.matcher(Pattern.quote(grokpattern));
         if (m.matches()) {
             String whole = m.group("WHOLE");
